e2d26a7cf85c515304c722da2fe914aa787e2467
LUIS DAVID HERNANDEZ NAVARRO <daezhn@gmail.com>
2025-10-17 14:09:57 -0600

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,13 @@
-import app from "../server/index";
+import express from "express";
+import { registerRoutes } from "../server/routes";
 
-export default app;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const setupPromise = registerRoutes(app);
+
+export default async function handler(req: any, res: any) {
+	await setupPromise;
+	return app(req as any, res as any);
+}
